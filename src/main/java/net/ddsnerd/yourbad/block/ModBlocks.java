906c81576ac0cfea1bc0a09a5a93758739ce202e
2D <67228925+2DSNerd@users.noreply.github.com>
2023-05-29 20:12:14 -0500

    Update ModBlocks.java

--- src/main/java/net/ddsnerd/yourbad/block/ModBlocks.java
+++ src/main/java/net/ddsnerd/yourbad/block/ModBlocks.java
@@ -3,36 +3,37 @@ package net.ddsnerd.yourbad.block;
 import net.ddsnerd.yourbad.item.ModItemGroup;
 import net.ddsnerd.yourbad.yourbad;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.block.Material;
-import net.minecraft.block.OreBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.PickaxeItem;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-
 
 public class ModBlocks {
 
     public static final Block END_TITANIUM_ORE = registerBlock("end_titanium_ore",
-            new OreBlock(FabricBlockSettings.of(Material.STONE).strength(4.5f).requiresTool()), ModItemGroup.SCYTHESORE);
+            new ExperienceDroppingBlock(FabricBlockSettings.of(Material.STONE).strength(4.5f).requiresTool()),
+            ModItemGroup.SCYTHESORE);
 
-    private static Block registerBlock(String name, Block block, ItemGroup group){
+    private static Block registerBlock(String name, Block block, ItemGroup group) {
         registerBlockItem(name, block, group);
-        return Registry.register(Registry.BLOCK, new Identifier(yourbad.MOD_ID, name), block);
+        return Registry.register(Registries.BLOCK, new Identifier(yourbad.MOD_ID, name), block);
     }
 
-
     private static Item registerBlockItem(String name, Block block, ItemGroup group) {
-        return Registry.register(Registry.ITEM, new Identifier(yourbad.MOD_ID, name),
-                new BlockItem(block, new FabricItemSettings().group(group)));
+        Item item = Registry.register(Registries.ITEM, new Identifier(yourbad.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings()));
+        ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
+        return item;
     }
 
     public static void registerModBlocks() {
-        yourbad.LOGGER.info("Registering ModBlocks for" + yourbad.MOD_ID);
+        yourbad.LOGGER.info("Registering ModBlocks for " + yourbad.MOD_ID);
     }
-
 }
