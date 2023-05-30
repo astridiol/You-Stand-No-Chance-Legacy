4aaf8f6fc03e83a9cb6f0648d05dd47be622d94b
2D <67228925+2DSNerd@users.noreply.github.com>
2023-05-29 20:13:10 -0500

    Update ModItemGroup.java

--- src/main/java/net/ddsnerd/yourbad/item/ModItemGroup.java
+++ src/main/java/net/ddsnerd/yourbad/item/ModItemGroup.java
@@ -1,13 +1,25 @@
-//Makes A Custom Item Group
 package net.ddsnerd.yourbad.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.ddsnerd.yourbad.yourbad;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ModItemGroup {
-    public static final ItemGroup SCYTHES = FabricItemGroupBuilder.build(new Identifier(yourbad.MOD_ID, "scythes"), () -> new ItemStack(ModItems.NETHERITESCYTHE));
-   public static final ItemGroup SCYTHESORE = FabricItemGroupBuilder.build(new Identifier(yourbad.MOD_ID, "scytheore"),  () -> new ItemStack(ModItems.TITANIUM_INGOT));
+    public static ItemGroup SCYTHES;
+    public static ItemGroup SCYTHESORE;
+
+    public static void registerItemGroups() {
+
+
+
+        SCYTHES = FabricItemGroup.builder(new Identifier(yourbad.MOD_ID, "scythes"))
+                .displayName(Text.literal("You Stand No Chance Pt. 1"))
+                .icon(() -> new ItemStack(ModItems.NETHERITESCYTHE)).build();
+        SCYTHESORE = FabricItemGroup.builder(new Identifier(yourbad.MOD_ID, "scytheore"))
+                .displayName(Text.literal("You Stand No Chance Pt. 2 (Don't Use)"))
+                .icon(() -> new ItemStack(ModItems.TITANIUM_INGOT)).build();
+    }
 }
