858b45d04276fffd1489c020790a71c3496c7905
2D <67228925+2DSNerd@users.noreply.github.com>
2023-08-31 21:04:49 -0500

    Update ModBlocks.java

--- src/main/java/net/ddsnerd/yourbad/block/ModBlocks.java
+++ src/main/java/net/ddsnerd/yourbad/block/ModBlocks.java
@@ -1,5 +1,3 @@
-package net.ddsnerd.yourbad.block;
-
 import net.ddsnerd.yourbad.item.ModItemGroup;
 import net.ddsnerd.yourbad.yourbad;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
@@ -7,29 +5,26 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.ExperienceDroppingBlock;
-import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.block.*;
 
 public class ModBlocks {
 
     public static final Block END_TITANIUM_ORE = registerBlock("end_titanium_ore",
-            new ExperienceDroppingBlock(FabricBlockSettings.of(Material.STONE).strength(4.5f).requiresTool()),
-            ModItemGroup.SCYTHESORE);
+            new Block(FabricBlockSettings.copyOf(Blocks.STONE).strength(4.5f).requiresTool()));
 
-    private static Block registerBlock(String name, Block block, ItemGroup group) {
-        registerBlockItem(name, block, group);
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
         return Registry.register(Registries.BLOCK, new Identifier(yourbad.MOD_ID, name), block);
     }
 
-    private static Item registerBlockItem(String name, Block block, ItemGroup group) {
+    private static Item registerBlockItem(String name, Block block) {
         Item item = Registry.register(Registries.ITEM, new Identifier(yourbad.MOD_ID, name),
                 new BlockItem(block, new FabricItemSettings()));
-        ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
         return item;
     }
 
