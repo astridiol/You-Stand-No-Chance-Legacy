25ca6727f15a21ef26f9a26bf0ccfafc2d07d6ed
2D <67228925+2DSNerd@users.noreply.github.com>
2023-08-31 21:04:23 -0500

    Fixes Problems With Groups

--- src/main/java/net/ddsnerd/yourbad/item/ModItemGroup.java
+++ src/main/java/net/ddsnerd/yourbad/item/ModItemGroup.java
@@ -1,25 +1,42 @@
 package net.ddsnerd.yourbad.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.ddsnerd.yourbad.yourbad;
+import net.ddsnerd.yourbad.block.ModBlocks;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ModItemGroup {
-    public static ItemGroup SCYTHES;
-    public static ItemGroup SCYTHESORE;
+    public static ItemGroup SCYTHES = Registry.register(Registries.ITEM_GROUP, new Identifier(yourbad.MOD_ID, "yourbad"),
+            FabricItemGroup.builder().displayName(Text.translatable("yourbad.scythes"))
+                    .icon(() -> new ItemStack(ModItems.NETHERITESCYTHE)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.WOODSCYTHE);
+                        entries.add(ModItems.STONESCYTHE);
+                        entries.add(ModItems.IRONSCYTHE);
+                        entries.add(ModItems.GOLDSCYTHE);
+                        entries.add(ModItems.DIAMONDSCYTHE);
+                        entries.add(ModItems.NETHERITESCYTHE);
+                        entries.add(ModItems.TITANIUMSCYTHE);
+
+                        entries.add(ModItems.TITAINIUMHELMET);
+                        entries.add(ModItems.TITAINIUMCHESTPLATE);
+                        entries.add(ModItems.TITAINIUMLEGGINGS);
+                        entries.add(ModItems.TITAINIUMBOOTS);
+
+                        entries.add(ModBlocks.END_TITANIUM_ORE);
+                        entries.add(ModItems.TITANIUM_INGOT);
+                        entries.add(ModItems.TITANIUM_NUGGET);
+                    }).build());
 
     public static void registerItemGroups() {
-
-
-
-        SCYTHES = FabricItemGroup.builder(new Identifier(yourbad.MOD_ID, "scythes"))
-                .displayName(Text.literal("You Stand No Chance Pt. 1"))
-                .icon(() -> new ItemStack(ModItems.NETHERITESCYTHE)).build();
-        SCYTHESORE = FabricItemGroup.builder(new Identifier(yourbad.MOD_ID, "scytheore"))
-                .displayName(Text.literal("You Stand No Chance Pt. 2 (Don't Use)"))
-                .icon(() -> new ItemStack(ModItems.TITANIUM_INGOT)).build();
+        // Example of adding to existing Item Group
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+        });
     }
 }
